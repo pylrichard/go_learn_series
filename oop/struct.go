3e02e45538f209b0c4811bed4bc4ad61a4f23598
pylrichard <pylrichard@qq.com>
2021-05-17 13:16:29 +0800

    5-面向对象

---
+++ oop/struct.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+//Go不支持继承
+type Person struct {
+	Name string
+	Age  int
+}
+
+//对象成员进行值复制
+func (p Person) toString() string {
+	return "Name: " + p.Name
+}
+
+//接收者为指针，避免对象成员进行内存拷贝
+func (p *Person) String() string {
+	return "Name: " + p.Name
+}
+
+func main() {
+	p1 := Person{
+		Name:	"pyl",
+		Age:	33,
+	}
+	fmt.Println(p1)
+	//返回对象指针，相当于p2 := &Person{}
+	p2 := new(Person)
+	p2.Name = "zy"
+}
