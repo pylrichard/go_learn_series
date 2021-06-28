73c3944a36195d0d26c5f23d17c8e51c0d8102ba
pylrichard <pylrichard@qq.com>
2021-06-28 19:46:24 +0800

    32-优雅退出

---
+++ quit/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/user", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello user")
+	})
+
+	s := &http.Server{
+		Addr: "8080",
+		Handler: r,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil &&
+			err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	log.Println("shutdown...")
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancelFunc()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("server shutdown:", err)
+	}
+	log.Println("server exit")
+}
